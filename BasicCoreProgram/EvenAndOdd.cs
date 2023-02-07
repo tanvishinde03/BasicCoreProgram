7dcea6bafd5faf5fe6adb1741de76b2c7d80eee8
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-07 23:03:14 +0530

    [Tanvi] Add : UC8 - Even And Odd

---
+++ BasicCoreProgram/EvenAndOdd.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public  class EvenAndOdd
+    {
+
+        public void checkevenorodd()
+        {
+            int Number;
+            Console.Write("Enter The Number: ");
+            Number = int.Parse(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+            }
+            Console.Read();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
